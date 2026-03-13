43fc44fcd903faa03bca0c24f183ce3480b636ec
opedrorenato <pedroramos3225@gmail.com>
2026-03-13 17:24:26 -0300

    Adicionando CORS

--- Backend/OrderManager/OrderGenerator/Program.cs
+++ Backend/OrderManager/OrderGenerator/Program.cs
@@ -14,6 +14,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 
 // Swagger
@@ -64,5 +73,7 @@ app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
+app.UseCors("AllowAll");
 
 await app.RunAsync();
